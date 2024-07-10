6a107b3844ee8438a2f641c0f614d2e8e006b35f
TheBadLagMan <supersmashmaster9000@gmail.com>
2024-07-10 19:49:19 -0400

    Extensive rework of code
     Added support for edge cases and refined code.

--- main.js
+++ main.js
@@ -18,59 +18,126 @@ client.on("ready", () => {
 let botID = process.env.CLIENTID
 client.on("messageCreate", (message) =>{
 
-//Try catch statement is to auto-rstart bot if any errors occur
-try {
-  if (message.content.includes("https://www.x.com") || message.content.includes("https://x.com"))
-    {
-
-      if ((message.content.includes("vxtwitter.com") || message.content.includes("fxtwitter.com") || message.content.includes("fixupx.com")) || (message.author.id == botID))
-      {
-        return;
-      }
-
-      else
-      {
-        newmsg = message.content.replaceAll("x.com", "fixupx.com")
-        message.reply("**Fixed message with embed:\n**" + newmsg + "\n" + "***To do twitter embeds replace x.com and twitter.com in your message with fixupx.com or fxtwitter.com respectively***");
-      }
-    }
-
-    if (message.content.includes("https://www.twitter.com") || message.content.includes("https://twitter.com"))
-    {
-      if((message.content.includes("fxtwitter.com") || message.content.includes("vxtwitter.com") || message.content.includes("fixupx.com")) || (message.author.id == botID))
-      {
-        return;
-      }
-
-      else
-      {
-        newmsg = message.content.replaceAll("twitter.com", "fxtwitter.com")
-        message.reply("**Fixed message with embed:\n**" + newmsg + "\n" + "***To do twitter embeds replace x.com and twitter.com in your message with fixupx.com or fxtwitter.com respectively***");
-      }
-    }
-
-    if (message.content.includes("https://www.tiktok.com") || message.content.includes("https://tiktok.com"))
-    {
-
-      if (message.content.includes("tfxktok.com") || (message.author.id == botID))
-      {
-        return;
-      }
-
-      else
-      {
-        newmsg = message.content.replaceAll("tiktok.com", "tfxktok.com")
-        message.reply("**Fixed message with embed:\n**" + newmsg + "\n" + "***To do tiktok embeds tiktok.com in your message with tfxktok.com \nYou can type s/e/fx on desktop to edit your previous sent message to include the embed***");
-      }
-    }
-  } 
-
-  catch (error) 
+//Try catch statement is to auto-restart bot if any errors occur
+try { 
+  
+  //Ignores messages sent by bot
+  if (message.author == botID)
   {
-    console.log("Fatal error detected1 Restarting bot!")
-    client.login(process.env.TOKEN)
+    return
   }
+
+  else
+  {
+    let msg = message.content
+
+    //Sets all chars to lowercase to easily check if message has a link
+    let lowerMsg = message.content.toLowerCase()
+
+    if(lowerMsg.includes("https://" ) && lowerMsg.includes(".com"))
+    {
+
+      //Calls function to collect alll twitter, x, and tiktok links
+      let returnedLinks = (fixLinks(msg))
+
+      //If no matching links are found, do nothing
+      if ((returnedLinks == 0) || (returnedLinks == undefined) || (returnedLinks == null) || (returnedLinks == "") || (returnedLinks == " "))
+      {
+        return
+      }
+
+      else
+      {
+      //Modifies all relevant links to include embeds and reply to original message
+      let fixedmsg = returnedLinks.toString()
+      fixedmsg = fixedmsg.replace(/twitter.com/gi, "fxtwitter.com")
+      fixedmsg = fixedmsg.replace(/x.com/gi, "fixupx.com")
+      fixedmsg = fixedmsg.replace(/tiktok.com/gi, "tfxktok.com")
+      message.reply("**Fixed embeds in message:\n**" + fixedmsg + "\n" + "***To do twitter embeds replace x.com and twitter.com in your message with fixupx.com or fxtwitter.com respectively\nTo do tiktok embeds replace tiktok.com in your message with tfxktok.com \nHint: You can type s/e/fx on desktop to edit your previous sent message to include the TikTok embed***");}
+      }
+
+    else 
+    {
+      return
+    }
+  }
+} 
+
+//If an error is caught, the bot is restarted
+catch (error) 
+{
+  console.log("Fatal error detected, Restarting bot!")
+  console.log(error)
+  client.login(process.env.TOKEN)
+}
 })
+
+//Function to isolate each links and modify them to include embeds.
+function fixLinks(newmsg)
+{
+  //Another lowercase instance of the message. This is for comparision within the function.
+  let lowerMsg2 = newmsg.toLowerCase()
+  
+  //Array decleration. All links with fixable embeds are pushed here
+  const linksToFix = []
+
+  //For loop going through string to see if a link is detected
+  for (j = 0; j < newmsg.length + 1; j++)
+  {
+    //Gets the base link from https:// and the .com indices
+    //+3 is added to .com as location is taken from start of substring
+    linkStart = lowerMsg2.indexOf("https://")
+    dotComLocation = lowerMsg2.indexOf(".com")
+    dotComLocation += 4
+            
+    //Finds full link after the .com
+    for (i = dotComLocation; i < newmsg.length + 1; i++)
+      {
+        if ((newmsg.charAt(i) == null) || (newmsg.charAt(i) == undefined) || (newmsg.charAt(i) == "") || (newmsg.charAt(i) == " "))
+        {
+          linkEnd = i
+          break
+        }
+      }
+      
+    //Checks if the link is a Twitter, X, or Tiktok link
+    testLink = newmsg.slice(linkStart, linkEnd + 1)
+    testLinkLower = testLink.toLowerCase()
+    if(testLinkLower.includes("twitter.com") || testLinkLower.includes("x.com") || testLinkLower.includes("tiktok.com"))
+    {
+
+      //vxtwitter.com is also a Twitter embed fix, if it's caught because of the character overlap with twitter.com, it is skipped
+      if(testLinkLower.includes("vxtwitter.com"))
+      {
+        j += testLink.length
+        return
+      }
+
+      if(testLinkLower.includes("fixupx.com"))
+      {
+        j += testLink.length
+        return
+      }
+
+      else
+      {    
+        //Pushes link to array, removes link from newmsg and resets loop count to search for next instance of a link
+        linksToFix.push(testLink);
+        newmsg = newmsg.replace(testLink, "")
+        newmsg = newmsg.trim()
+        j = 0
+      }
+    }
+
+    else
+    {
+      j += testLink.length
+    }
+  }    
+
+  //Links are sent back to main body where they will be edited and posted
+  return linksToFix
+}
 
 //Bot token for login
 client.login(process.env.TOKEN)
